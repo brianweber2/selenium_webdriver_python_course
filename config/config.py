4a155fcea6dd0061175d8c30732f5e6062d024b2
Brian Weber <brian_weber@intuit.com>
2020-11-27 18:34:18 -0800

    Update config file.

--- config/config.py
+++ config/config.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 
 
+# Web Drivers
 CHROME_DRIVER_EXECUTABLE_PATH = '/opt/selenium/chromedriver'
+CHROME_DRIVER = webdriver.Chrome(executable_path=CHROME_DRIVER_EXECUTABLE_PATH)
+
+# URLs
 BRIANS_WEBSITE_URL = 'https://brianwweber.com'
 SALESFORCE_LOGIN_URL = 'https://login.salesforce.com/'
 RAHUL_SHETTY_URL = 'https://rahulshettyacademy.com'
-CHROME_DRIVER = webdriver.Chrome(executable_path=CHROME_DRIVER_EXECUTABLE_PATH)
