526c974f65dea3bc4aae57db4ca9bf47d848312f
Brian Weber <brian_weber@intuit.com>
2021-03-11 14:51:35 -0800

    Add more assertions for checking cart items and item costs.

--- python_selenium/explicit_wait_demo.py
+++ python_selenium/explicit_wait_demo.py
@@ -8,6 +8,8 @@ from config.config import CHROME_DRIVER, RAHUL_SHETTY_URL
 
 
 if __name__ == '__main__':
+    product_names_in_cart = []
+
     CHROME_DRIVER.get(f'{RAHUL_SHETTY_URL}/seleniumPractise/')
     CHROME_DRIVER.find_element(By.CSS_SELECTOR, 'input.search-keyword').send_keys('ber')
 
@@ -19,7 +21,9 @@ if __name__ == '__main__':
     # Add products to cart.
     buttons = CHROME_DRIVER.find_elements(By.XPATH, "//div[@class='product-action']/button")
     for button in buttons:
+        product_names_in_cart.append(button.find_element(By.XPATH, "parent::div/parent::div/h4").text)
         button.click()
+    print(product_names_in_cart)
 
     # Proceed to checkout.
     CHROME_DRIVER.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
@@ -28,13 +32,30 @@ if __name__ == '__main__':
     wait.until(
         EC.presence_of_element_located((By.CLASS_NAME, "promoCode"))
     )
+    veggies = CHROME_DRIVER.find_elements(By.CSS_SELECTOR, 'p.product-name')
+    product_names_on_cart_page = [v.text for v in veggies]
+    print(product_names_on_cart_page)
+    assert product_names_in_cart == product_names_on_cart_page
+
     # Apply discount.
+    original_amount = float(CHROME_DRIVER.find_element(By.CSS_SELECTOR, '.discountAmt').text)
+    print(f'Original amount: ${original_amount}')
     CHROME_DRIVER.find_element(By.CLASS_NAME, 'promoCode').send_keys('rahulshettyacademy')
     CHROME_DRIVER.find_element(By.CSS_SELECTOR, '.promoBtn').click()
-
     wait.until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "span.promoInfo"))
     )
+    discount_amount = float(CHROME_DRIVER.find_element(By.CSS_SELECTOR, '.discountAmt').text)
+    print(f'Discount amount: ${discount_amount}')
+    assert discount_amount < original_amount
     discount_success_text = CHROME_DRIVER.find_element(By.CSS_SELECTOR, 'span.promoInfo').text
+    print(discount_success_text)
+
+    amounts = [float(amount.text) for amount in CHROME_DRIVER.find_elements(By.XPATH, '//tr/td[5]/p')]
+    amounts_sum = sum(amounts)
+    print(amounts_sum)
+    total_amount = float(CHROME_DRIVER.find_element(By.CSS_SELECTOR, '.totAmt').text)
+    print(total_amount)
+    assert amounts_sum == total_amount
 
     CHROME_DRIVER.close()
